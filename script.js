cd5803a7cc34a28828f3dd925a9c7c12cf9faca7
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-10-16 23:15:12 +0300

    console answers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var countryList = {
-	'countries':[
+var countryList = [
+	
 		{
 			'name': 'Ukraine',
 			'capital': 'Kiev'
@@ -40,11 +40,54 @@ var countryList = {
 			'name': 'Norway',
 			'capital': 'Oslo'
 		}
-	] 
-}
+] 
+var questions = [
+	{
+		'label': 'Ukraine',
+		'options': ['Oslo', 'Kiev', 'Moscow', 'London'],
+		'answer': 'Kiev'
+	},
+	{
+		'label': 'England',
+		'options': ['Minsk', 'Oslo', 'London', 'Kiev'],
+		'answer': 'London'
+	},
+	{
+		'label': 'France',
+		'options': ['Paris', 'Kiev', 'Rome', 'London'],
+		'answer': 'Paris'
+	},
+	{
+		'label': 'Belarus',
+		'options': ['Oslo', 'Minsk', 'Baku', 'London'],
+		'answer': 'Minsk'
+	},
+	{
+		'label': 'Italy',
+		'options': ['Madrid', 'Kiev', 'London', 'Rome'],
+		'answer': 'Rome'
+	},
+	{
+		'label': 'Norway',
+		'options': ['Oslo', 'Kiev', 'Moscow', 'London'],
+		'answer': 'Oslo'
+	}
+];
+var answer;
 $(document).ready(function(){
-	var listLength = countryList.countries.length;
-	var counter = Math.floor(Math.random()*listLength);
-	$('#question').append('<p id="country">'+countryList.countries[counter].name+'</p>');
-	counter =  Math.floor(Math.random()*listLength);
+	var counter = Math.floor(Math.random()*questions.length);
+	$('#question').append(questions[counter].label);
+	$('#first').append(questions[counter].options[0]);
+	$('#second').append(questions[counter].options[1]);
+	$('#third').append(questions[counter].options[2]);
+	$('#fourth').append(questions[counter].options[3]);
+	$(document).on('click','#answers .answer', function(){
+		answer = $(this).text();
+		if(questions[counter].answer===answer){
+			console.log('true');
+		}
+		else{
+			console.log('false');
+		}
+	});
 });
